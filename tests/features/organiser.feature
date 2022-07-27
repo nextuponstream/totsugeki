ac355000f92d436e40d4987e2a23cbb316587299
TakeCare <takecare@pop-os.localdomain>
2022-07-27 10:53:59 +0200

    Rewrite test running two brackets for parallel execution

--- tests/features/organiser.feature
+++
@@ -1,7 +0,0 @@
-Feature: Generating organiser when creating bracket
-
-    Scenario: Organiser run another bracket
-        Given my-favorite-to wants to create a bracket named basel-weekly-return
-        When the organiser FancyBar has already ran a bracket named basel-weekly
-        When they create a bracket using discord bot
-        Then there is only one organiser with two brackets named basel-weekly and basel-weekly-return
--- totsugeki-api/tests/organiser.rs
+++ totsugeki-api/tests/organiser.rs
@@ -51,11 +51,6 @@ fn parse_organiser_get_response(response: TestJson) -> Vec<Organiser> {
         .collect()
 }
 
-// Feature: Generating organiser when creating bracket
-// Scenario: New organiser is generated when bracket is created
-//  ✔  Given my-favorite-to wants to create a bracket named basel-weekly
-//  ✔  When they create a bracket using discord bot
-//  ✔  Then there is a organiser named FancyBar with the new active bracket
 #[tokio::test]
 async fn new_organiser_is_generated_when_bracket_is_created_if_unknown() {
     for db_type in db_types_to_test() {
@@ -114,18 +109,93 @@ async fn new_organiser_is_generated_when_bracket_is_created_if_unknown() {
         test_api.clean_db().await;
     }
 }
-// Scenario: Organiser run another bracket
-//  ✔  Given my-favorite-to wants to create a bracket named basel-weekly-return
-//  ✔  When the organiser FancyBar has already ran a bracket named basel-weekly
-//  ✔  When they create a bracket using discord bot
-//  ✔  Then there is only one organiser with two brackets named basel-weekly and basel-weekly-return
+
 #[tokio::test]
-async fn running_consecutive_brackets() {
+async fn running_two_brackets_at_the_same_time() {
     for db_type in db_types_to_test() {
         let test_api = test_api(db_type).await;
-        todo!();
-        // let resp = test_api.cli.post("/bracket").send().await;
-        // resp.assert_status_is_ok();
+
+        // Given FancyBar wants to run two brackets at the same time for two different games
+        let bracket_name = "weekly-game-1".to_string(); // TODO generate name
+        let organiser_name = "FancyBar".to_string();
+        let organiser_internal_id = "1".to_string();
+        let channel_internal_id = "1".to_string();
+        let service_type_id = "discord".to_string();
+        let body = BracketPOST::new(
+            bracket_name,
+            organiser_name.clone(),
+            organiser_internal_id.clone(),
+            channel_internal_id,
+            service_type_id.clone(),
+        );
+
+        let bracket_name = "weekly-game-2".to_string(); // TODO generate name
+        let channel_internal_id = "2".to_string();
+        let body_next_bracket = BracketPOST::new(
+            bracket_name,
+            organiser_name.clone(),
+            organiser_internal_id,
+            channel_internal_id.clone(),
+            service_type_id,
+        );
+
+        // When they create brackets using discord bot
+        let resp = test_api
+            .cli
+            .post("/bracket")
+            .header("X-API-Key", test_api.authorization_header.as_str())
+            .body_json(&body)
+            .send()
+            .await;
+        resp.assert_status_is_ok();
+
+        let resp = resp.json().await;
+        let bracket_post_resp_game_1 = parse_bracket_post_response(resp);
+
+        let resp = test_api
+            .cli
+            .post("/bracket")
+            .header("X-API-Key", test_api.authorization_header.as_str())
+            .body_json(&body_next_bracket)
+            .send()
+            .await;
+        resp.assert_status_is_ok();
+
+        let resp = resp.json().await;
+        let bracket_post_resp_game_2 = parse_bracket_post_response(resp);
+
+        // Then there is only one organiser with two active brackets
+        let resp = test_api.cli.get("/organiser/0").send().await;
+        resp.assert_status_is_ok();
+
+        let r = resp.json().await;
+        let organisers = parse_organiser_get_response(r);
+
+        assert_eq!(
+            organisers.len(),
+            1,
+            "incorrect number of result, expected 1, actual: {}\n{organisers:?}",
+            organisers.len()
+        );
+
+        assert!(
+            organisers.iter().any(
+                |o| o.get_organiser_id() == bracket_post_resp_game_1.get_organiser_id()
+                    && o.get_organiser_name() == organiser_name.as_str()
+                    && o.get_active_brackets().iter().any(|b|
+                        b.0 == &bracket_post_resp_game_1.get_discussion_channel_id()
+                        && b.1 == &bracket_post_resp_game_1.get_bracket_id()
+                    )
+                    && o.get_active_brackets().iter().any(|b|
+                        b.0 == &bracket_post_resp_game_2.get_discussion_channel_id()
+                        && b.1 == &bracket_post_resp_game_2.get_bracket_id()
+                    )
+            ),
+            "no matching organiser id for weekly \"{}\", weekly-return \"{}\", with organiser name \"{organiser_name}\" in:\n {organisers:?}",
+            bracket_post_resp_game_1.get_organiser_id(),
+            bracket_post_resp_game_2.get_organiser_id()
+        );
+
         test_api.clean_db().await;
     }
 }
