4d084597bb92c79c728da5a33c0e52b74ecdc851
TakeCare <takecare@pop-os.localdomain>
2022-09-27 16:50:35 +0200

    Add create+join command

--- totsugeki-discord-bot/src/lib.rs
+++ totsugeki-discord-bot/src/lib.rs
@@ -1,3 +1,27 @@
 //! set of function to use
 
+use async_lock::RwLock;
+use serde::{Deserialize, Serialize};
+use serenity::{model::id::UserId, prelude::TypeMapKey};
+use std::{collections::HashMap, sync::Arc};
+use totsugeki::{bracket::Bracket, player::Player};
+
+pub mod create;
+pub mod join;
 pub mod ping;
+
+pub struct Config;
+
+impl TypeMapKey for Config {
+    type Value = Arc<String>;
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Data {
+    pub bracket: Bracket,
+    pub users: HashMap<UserId, Player>,
+}
+
+impl TypeMapKey for Data {
+    type Value = Arc<RwLock<(Bracket, HashMap<UserId, Player>)>>;
+}
--- totsugeki-discord-bot/src/main.rs
+++ totsugeki-discord-bot/src/main.rs
@@ -1,16 +1,27 @@
+use async_lock::RwLock;
 use serenity::async_trait;
 use serenity::framework::standard::macros::group;
 use serenity::framework::standard::StandardFramework;
 use serenity::prelude::*;
-use std::env;
-use totsugeki_discord_bot::ping::*;
+use std::io::Write;
+use std::path::Path;
+use std::sync::Arc;
+use std::{collections::HashMap, env};
+use totsugeki::bracket::Bracket;
+use totsugeki_discord_bot::{create::*, join::*, ping::*, Config, Data};
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::BunyanFormattingLayer;
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
+// static BRACKET: RwLock<Bracket> = RwLock::new(Bracket::default());
+
+// if you want to use Data, you need a dyn trait which implements boxed and
+// sync.
+// Or you can read from file everytime.
+
 #[group]
-#[commands(ping)]
+#[commands(ping, create, join)]
 #[summary = "Main available commands"]
 pub struct General;
 struct Handler;
@@ -37,6 +48,9 @@ async fn main() {
         // .help(&HELP)
         .group(&GENERAL_GROUP);
 
+    let bracket_filename =
+        env::var("BRACKET_FILENAME").expect("BRACKET_FILENAME environment variable");
+
     // Login with a bot token from the environment
     let token_path = env::var("DISCORD_BOT_TOKEN_PATH").expect("DISCORD_BOT_TOKEN_PATH");
     let token = std::fs::read_to_string(token_path).expect("discord agent token secret");
@@ -46,6 +60,31 @@ async fn main() {
         .framework(framework)
         .await
         .expect("Error creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<Config>(Arc::new(bracket_filename.clone()));
+        let p = Path::new(&bracket_filename);
+        let mut f = std::fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(p)
+            .expect("");
+        let bracket_data = std::fs::read(p).expect("file");
+        let bracket_data = match serde_json::from_slice::<Data>(&bracket_data) {
+            Ok(d) => d,
+            Err(_) => Data {
+                bracket: Bracket::default(),
+                users: HashMap::new(),
+            },
+        };
+        data.insert::<Data>(Arc::new(RwLock::new((
+            bracket_data.bracket.clone(),
+            bracket_data.users.clone(),
+        ))));
+        let j = serde_json::to_string(&bracket_data).expect("bracket");
+        f.write_all(j.as_bytes()).expect("write");
+    }
 
     // start listening for events by starting a single shard
     if let Err(why) = client.start().await {
