958040a194419419ffc34dcb2892f5cf64e4fde6
TakeCare <takecare@pop-os.localdomain>
2023-03-06 06:31:23 +0100

    Add bracket format input

---
+++ totsugeki-native-app/src/components/navigation.rs
@@ -0,0 +1,23 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+
+pub fn Navigation(cx: Scope) -> Element {
+    cx.render(rsx!(
+        div {
+            class: "grid grid-cols-3 gap-4 p-4 bg-blue-100",
+            div {
+                class: "text-start p-2",
+                format!("totsugeki-native-app v{}", env!("CARGO_PKG_VERSION")),
+            }
+            div {
+                class: "text-center bg-blue-200 p-2",
+                "Totsugeki!",
+            }
+            div {
+                class: "text-end p-2",
+                "Lang TODO",
+            }
+        }
+    ))
+}
