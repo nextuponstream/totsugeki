74d2d512718886d4b5f156faba991d5347b13615
TakeCare <takecare@pop-os.localdomain>
2023-03-06 06:36:42 +0100

    Use bracket format select input

--- totsugeki-native-app/src/components/bracket/visualizer.rs
+++ totsugeki-native-app/src/components/bracket/visualizer.rs
@@ -40,16 +40,19 @@ pub fn UpdateBracketDetails(cx: Scope) -> Element {
                     name: "name",
                 }
             }
-
+            
             div {
                 class: "pb-2",
-                label { "Format" }
-                input {
-                    class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                label { 
+                    class: "pr-2",
+                    "Format"
+                }
+                select {
                     name: "format",
+                    option { "single-elimination" }
+                    option { "double-elimination" }
                 }
             }
-
 
             div {
                 input {
