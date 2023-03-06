62072818b6494c4e9792a437d7c8b48a5ae799af
TakeCare <takecare@pop-os.localdomain>
2023-03-06 06:31:23 +0100

    Add bracket format input

---
+++ totsugeki-native-app/src/components/bracket/visualizer.rs
@@ -0,0 +1,86 @@
+#![allow(non_snake_case)]
+
+use chrono::{TimeZone, Utc};
+use dioxus::prelude::*;
+use totsugeki::{bracket::Bracket, format::Format};
+
+pub fn GeneralDetails(cx: Scope) -> Element {
+    let bracket = use_shared_state::<Bracket>(cx).expect("bracket");
+
+    let details = bracket.read().to_string();
+    let format = bracket.read().get_format().to_string();
+
+    cx.render(rsx!(div {
+        h1 {
+            class: "text-lg",
+            "General details"
+        }
+        p { details }
+        p { label {
+            class: "pr-2",
+            "Format:" }
+            format
+        }
+    }))
+}
+
+pub fn UpdateBracketDetails(cx: Scope) -> Element {
+    let bracket = use_shared_state::<Bracket>(cx).expect("bracket");
+
+    cx.render(rsx!(
+
+        form {
+            onsubmit: move |event| { update_bracket(bracket, event ) },
+
+            div {
+                class: "pb-2",
+                label { "Name" }
+                input {
+                    class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                    name: "name",
+                }
+            }
+
+            div {
+                class: "pb-2",
+                label { "Format" }
+                input {
+                    class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+                    name: "format",
+                }
+            }
+
+
+            div {
+                input {
+                    class: "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
+                    r#type: "submit",
+                },
+            }
+
+        }
+    ))
+}
+
+fn update_bracket(bracket: &UseSharedState<Bracket>, e: Event<FormData>) {
+    println!("{e:?}");
+    let name = e.values.get("name").expect("name");
+    let format = e.values.get("format").expect("format");
+    let is_valid = true;
+    let (format, is_valid) = match format.parse::<Format>() {
+        Ok(f) => (f, is_valid),
+        Err(_e) => (Format::default(), false),
+    };
+
+    if !is_valid {
+        return;
+    }
+
+    *bracket.write() = Bracket::new(
+        name,
+        format,
+        totsugeki::seeding::Method::Strict,
+        Utc.with_ymd_and_hms(2000, 1, 1, 0, 0, 0).unwrap(),
+        true,
+    );
+}
--- totsugeki-native-app/src/main.rs
+++ totsugeki-native-app/src/main.rs
@@ -1,10 +1,12 @@
 #![allow(non_snake_case)]
 // import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
-use chrono::{TimeZone, Utc};
-use dioxus::prelude::Event;
 use dioxus::prelude::*;
 use dioxus_desktop::Config;
 use totsugeki::bracket::Bracket;
+use totsugeki_native_app::components::{
+    bracket::visualizer::{GeneralDetails, UpdateBracketDetails},
+    navigation::Navigation,
+};
 
 fn main() {
     hot_reload_init!();
@@ -16,71 +18,18 @@ fn main() {
 }
 
 fn App(cx: Scope) -> Element {
-    let bracket = use_state(cx, Bracket::default);
+    use_shared_state_provider(cx, Bracket::default);
 
     cx.render(rsx! {
         style { include_str!("../resources/tailwind.css") }
-        div {
-            class: "grid grid-cols-3 gap-4 p-4 bg-blue-100",
-            div {
-                class: "text-start p-2",
-                format!("totsugeki-native-app v{}", env!("CARGO_PKG_VERSION")),
-            }
-            div {
-                class: "text-center bg-blue-200 p-2",
-                "Totsugeki!",
-            }
-            div {
-                class: "text-end p-2",
-                "Lang TODO",
-            }
-        }
-
-        form {
-            onsubmit: move |event| { update_bracket(bracket, event ) },
-            class: "p-1",
-
-            div {
-                class: "pb-2",
-                label { "Name" }
-                input {
-                    class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
-                    name: "name",
-                }
-            }
-
-            div {
-                class: "",
-                // TODO missing hover style
-                input {
-                    class: "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
-                    r#type: "submit",
-                },
-            }
-
-        }
-
 
         div {
-            h1 { "Bracket" }
-            div {
-                bracket.to_string()
-            }
+            class: "pl-2",
+
+            Navigation {}
+            UpdateBracketDetails {}
+            GeneralDetails {}
         }
+
     })
-}
-
-fn update_bracket(bracket: &UseState<Bracket>, e: Event<FormData>) {
-    println!("{e:?}");
-    let name = e.values.get("name").expect("name");
-
-    bracket.set(Bracket::new(
-        name,
-        totsugeki::format::Format::DoubleElimination,
-        totsugeki::seeding::Method::Strict,
-        Utc.with_ymd_and_hms(2000, 1, 1, 0, 0, 0).unwrap(),
-        true,
-    ));
-
-    println!("{bracket}");
 }
