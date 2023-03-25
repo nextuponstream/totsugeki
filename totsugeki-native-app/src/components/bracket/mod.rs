bc7caf231e50c8b2698e5e3fe4f7371ea0bcb3da
TakeCare <takecare@pop-os.localdomain>
2023-03-25 22:29:02 +0100

    WIP

--- totsugeki-native-app/src/components/bracket/mod.rs
+++ totsugeki-native-app/src/components/bracket/mod.rs
@@ -1,3 +1,4 @@
+pub mod add_player;
 mod displayable_match;
 mod match_edit;
 pub mod visualizer;
--- totsugeki-native-app/src/main.rs
+++ totsugeki-native-app/src/main.rs
@@ -4,22 +4,23 @@ use chrono::prelude::*;
 use dioxus::prelude::*;
 use dioxus_desktop::Config;
 use totsugeki::bracket::Bracket;
-use totsugeki::matches::Id as MatchId;
 use totsugeki_native_app::components::{
-    bracket::visualizer::{AddPlayerForm, GeneralDetails, UpdateBracketDetails, View},
+    bracket::add_player::Form as AddPlayerForm,
+    bracket::visualizer::{GeneralDetails, UpdateBracketDetails, View},
     navigation::Navigation,
 };
+use totsugeki_native_app::Modal;
 
 fn main() {
     hot_reload_init!();
     // TODO add some auto-completion for css class
     // wait for https://github.com/helix-editor/helix/issues/2213
     // TODO i18n (fluent crate)
-    // TODO minify used css
     dioxus_desktop::launch_cfg(App, Config::new());
 }
 
 fn App(cx: Scope) -> Element {
+    dioxus_desktop::use_window(cx).set_title("Totsugeki bracket viewer");
     let b = Bracket::new(
         "test",
         totsugeki::format::Format::SingleElimination,
@@ -28,7 +29,7 @@ fn App(cx: Scope) -> Element {
         true,
     );
     use_shared_state_provider(cx, || b); // TODO revert to Bracket::default
-    use_shared_state_provider::<Option<MatchId>>(cx, || None);
+    use_shared_state_provider::<Option<Modal>>(cx, || None);
 
     cx.render(rsx! {
         style { include_str!("../resources/tailwind.css") }
@@ -37,9 +38,9 @@ fn App(cx: Scope) -> Element {
             class: "pl-2",
 
             Navigation {}
+            View {}
             UpdateBracketDetails {}
             AddPlayerForm {}
-            View {}
             GeneralDetails {}
         }
 
