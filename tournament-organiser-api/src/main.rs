3fa301c874695dcd286e6028e84e83df9a18fd2b
TakeCare <takecare@pop-os.localdomain>
2023-07-10 17:01:30 +0200

    Added page with sortable list

--- tournament-organiser-api/src/main.rs
+++ tournament-organiser-api/src/main.rs
@@ -30,77 +30,78 @@ async fn main() {
         .init();
 
     tokio::join!(
-        serve(using_serve_dir(), 3001),
-        serve(using_serve_dir_with_assets_fallback(), 3002),
-        serve(using_serve_dir_only_from_root_via_fallback(), 3003),
-        serve(using_serve_dir_with_handler_as_service(), 3004),
-        serve(two_serve_dirs(), 3005),
-        serve(calling_serve_dir_from_a_handler(), 3006),
+        // serve(using_serve_dir(), 3000),
+        serve(using_serve_dir_with_assets_fallback(), 3000),
+        // serve(using_serve_dir_only_from_root_via_fallback(), 3003),
+        // serve(using_serve_dir_with_handler_as_service(), 3004),
+        // serve(two_serve_dirs(), 3005),
+        // serve(calling_serve_dir_from_a_handler(), 3006),
     );
 }
 
-fn using_serve_dir() -> Router {
-    // serve the file in the "assets" directory under `/assets`
-    Router::new().nest_service("/assets", ServeDir::new("assets"))
-}
+// fn using_serve_dir() -> Router {
+//     // serve the file in the "assets" directory under `/assets`
+//     Router::new().nest_service("/assets", ServeDir::new("../tournament-organiser-web/dist"))
+// }
 
 fn using_serve_dir_with_assets_fallback() -> Router {
     // `ServeDir` allows setting a fallback if an asset is not found
     // so with this `GET /assets/doesnt-exist.jpg` will return `index.html`
     // rather than a 404
-    let serve_dir = ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
+    let serve_dir = ServeDir::new("../tournament-organiser-web/dist")
+        .not_found_service(ServeFile::new("dist/index.html"));
 
     Router::new()
         .route("/foo", get(|| async { "Hi from /foo" }))
-        .nest_service("/assets", serve_dir.clone())
+        .nest_service("/dist", serve_dir.clone())
         .fallback_service(serve_dir)
 }
 
-fn using_serve_dir_only_from_root_via_fallback() -> Router {
-    // you can also serve the assets directly from the root (not nested under `/assets`)
-    // by only setting a `ServeDir` as the fallback
-    let serve_dir = ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
+// fn using_serve_dir_only_from_root_via_fallback() -> Router {
+//     // you can also serve the assets directly from the root (not nested under `/assets`)
+//     // by only setting a `ServeDir` as the fallback
+//     let serve_dir = ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
 
-    Router::new()
-        .route("/foo", get(|| async { "Hi from /foo" }))
-        .fallback_service(serve_dir)
-}
+//     Router::new()
+//         .route("/foo", get(|| async { "Hi from /foo" }))
+//         .fallback_service(serve_dir)
+// }
 
-fn using_serve_dir_with_handler_as_service() -> Router {
-    async fn handle_404() -> (StatusCode, &'static str) {
-        (StatusCode::NOT_FOUND, "Not found")
-    }
+// fn using_serve_dir_with_handler_as_service() -> Router {
+//     async fn handle_404() -> (StatusCode, &'static str) {
+//         (StatusCode::NOT_FOUND, "Not found")
+//     }
 
-    let serve_dir = ServeDir::new("assets").not_found_service(handle_404.into_service());
+//     let serve_dir = ServeDir::new("assets").not_found_service(handle_404.into_service());
 
-    Router::new()
-        .route("/foo", get(|| async { "Hi from /foo" }))
-        .fallback_service(serve_dir)
-}
+//     Router::new()
+//         .route("/foo", get(|| async { "Hi from /foo" }))
+//         .fallback_service(serve_dir)
+// }
 
-fn two_serve_dirs() -> Router {
-    // you can also have two `ServeDir`s nested at different paths
-    let serve_dir_from_assets = ServeDir::new("assets");
-    let serve_dir_from_dist = ServeDir::new("dist");
+// fn two_serve_dirs() -> Router {
+//     // you can also have two `ServeDir`s nested at different paths
+//     let serve_dir_from_assets = ServeDir::new("assets");
+//     let serve_dir_from_dist = ServeDir::new("dist");
 
-    Router::new()
-        .nest_service("/assets", serve_dir_from_assets)
-        .nest_service("/dist", serve_dir_from_dist)
-}
+//     Router::new()
+//         .nest_service("/assets", serve_dir_from_assets)
+//         .nest_service("/dist", serve_dir_from_dist)
+// }
 
-#[allow(clippy::let_and_return)]
-fn calling_serve_dir_from_a_handler() -> Router {
-    // via `tower::Service::call`, or more conveniently `tower::ServiceExt::oneshot` you can
-    // call `ServeDir` yourself from a handler
-    Router::new().nest_service(
-        "/foo",
-        get(|request: Request<Body>| async {
-            let service = ServeDir::new("assets");
-            let result = service.oneshot(request).await;
-            result
-        }),
-    )
-}
+// #[allow(clippy::let_and_return)]
+// fn calling_serve_dir_from_a_handler() -> Router {
+//     // via `tower::Service::call`, or more conveniently `tower::ServiceExt::oneshot` you can
+//     // call `ServeDir` yourself from a handler
+//     Router::new().nest_service(
+//         "/foo",
+//         get(|request: Request<Body>| async {
+//             let service = ServeDir::new("assets");
+//             let result = service.oneshot(request).await;
+//             result
+//         }),
+//     )
+// }
 
 async fn serve(app: Router, port: u16) {
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
---
+++ tournament-organiser-web/src/components/TheWelcome.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+import draggable from 'vuedraggable'
+
+// https://github.com/SortableJS/vue.draggable.next/blob/master/example/components/simple.vue
+export default {
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      list: [
+        {id:1, name: "jean"},
+        {id:2, name: "jean2"},
+      ],
+      dragging: false,
+      enabled: true,
+    }
+  },
+}
+</script>
+
+<template>
+  <p>
+      <draggable 
+        :list="list" 
+        :disabled="!enabled"
+        item-key="name"
+        class="list-group"
+        ghost-class="ghost"
+        @start="dragging = true"
+        @end="dragging = false;console.log(JSON.stringify(list))"
+      >
+      <template #item="{ element }">
+          <div class="list-group-item" :class="{ 'not-draggable': !enabled }">
+            {{ element.name }}
+          </div>
+        </template>
+      </draggable>
+  </p>
+</template>
