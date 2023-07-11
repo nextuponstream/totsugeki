f0d708fc84943f24b2ea9bac3435a6c10961d2ff
TakeCare <takecare@pop-os.localdomain>
2023-07-11 17:55:23 +0200

    Remove bloat

--- tournament-organiser-web/src/views/HomeView.vue
+++ tournament-organiser-web/src/views/HomeView.vue
@@ -1,9 +1,46 @@
+// View to create bracket
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import draggable from 'vuedraggable'
+import { onMounted } from 'vue'
+import { ref } from 'vue'
+
+// https://github.com/SortableJS/vue.draggable.next/blob/master/example/components/simple.vue
+onMounted(async () => {
+  const response = await fetch("http://localhost:3000/foo");
+  if (response.ok) {
+    const text = await response.text();
+    console.log(text);
+  } else {
+    console.log('oh no', response.body)
+  }
+});
+
+const list = ref([
+  {id: 1, name: 'jean'},
+  {id: 2, name: 'jean2'},
+])
+
+const dragging = ref(false)
+const enabled = ref(true)
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <p>
+      <draggable 
+        :list="list" 
+        :disabled="!enabled"
+        item-key="name"
+        class="list-group"
+        ghost-class="ghost"
+        @start="dragging = true"
+        @end="dragging = false;console.log(JSON.stringify(list))"
+      >
+      <template #item="{ element }">
+          <div class="list-group-item" :class="{ 'not-draggable': !enabled }">
+            {{ element.name }}
+          </div>
+        </template>
+      </draggable>
+  </p>
 </template>
