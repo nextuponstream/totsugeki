e43ebc9deabdf4a924af0acfe61b9799ecb151cf
TakeCare <takecare@pop-os.localdomain>
2023-07-16 18:05:43 +0200

    Add test

--- tournament-organiser-web/cypress/e2e/homepage.cy.ts
+++ tournament-organiser-web/cypress/e2e/homepage.cy.ts
@@ -3,4 +3,9 @@ describe('Homepage proposes you to create a bracket', () => {
     cy.visit('/')
     cy.contains('Create a new bracket')
   })
+  it('About page is reachable', () => {
+    cy.visit('/')
+    cy.get('[data-test-id=about]').click()
+    cy.url().should('contain', '/about')
+  })
 })
