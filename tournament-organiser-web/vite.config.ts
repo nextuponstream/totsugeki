e166f2b56de7df3230be76a91deb66e1ddf881ae
TakeCare <takecare@pop-os.localdomain>
2023-07-17 18:52:21 +0200

    Add locales switch

--- tournament-organiser-web/vite.config.ts
+++ tournament-organiser-web/vite.config.ts
@@ -2,15 +2,26 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_I18N_FULL_INSTALL__: true,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  },
   plugins: [
     vue(), 
+    VueI18nPlugin({ 
+      include: [path.resolve(__dirname, './src/locales/**')],
+     }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+/*       'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
+ */    }
   }
 })
