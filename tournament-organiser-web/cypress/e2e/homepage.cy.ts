f8c129649eaf79e370dc6f0f059eea68cbb508f7
TakeCare <takecare@pop-os.localdomain>
2023-07-18 18:02:17 +0200

    Fix test

--- tournament-organiser-web/cypress/e2e/homepage.cy.ts
+++ tournament-organiser-web/cypress/e2e/homepage.cy.ts
@@ -1,7 +1,7 @@
 describe('Homepage proposes you to create a bracket', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('Create a new bracket')
+    cy.contains('Create new bracket')
   })
   it('About page is reachable', () => {
     cy.visit('/')
