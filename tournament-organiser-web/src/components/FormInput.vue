6f1225f4f4c99b566c46fede674f7cdb0e424e94
TakeCare <takecare@pop-os.localdomain>
2023-07-18 18:00:37 +0200

    Style player registration form

---
+++ tournament-organiser-web/src/components/FormInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    :placeholder="placeholder"
+    :value="m"
+    class="mt-1 block rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring
+        focus:ring-indigo-200 focus:ring-opacity-50"
+    @input="update"
+  >
+</template>
+<script setup lang="ts">
+const props = defineProps({
+    placeholder: { type: String, default: ''},
+    m: { type: String, default: ''},
+})
+const emit = defineEmits(['update:m'])
+
+function update($event: any){
+    emit('update:m', $event.target.value)
+}
+</script>
