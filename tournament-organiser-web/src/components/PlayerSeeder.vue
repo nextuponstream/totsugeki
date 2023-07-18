48263820089b83a80f838a56f464a54817e8602f
TakeCare <takecare@pop-os.localdomain>
2023-07-18 18:20:37 +0200

    More styling

--- tournament-organiser-web/src/components/PlayerSeeder.vue
+++ tournament-organiser-web/src/components/PlayerSeeder.vue
@@ -2,32 +2,39 @@
   <div class="text-3xl">
     Seeder
   </div>
-  <p>
-    <draggable 
-      :list="players" 
-      :disabled="!enabled"
-      item-key="name"
-      class="list-group"
-      ghost-class="ghost"
-      @start="dragging = true"
-      @end="dragging = false"
-    >
-      <template #item="{ element }">
-        <div
-          class="list-group-item"
-          :class="{ 'not-draggable': !enabled }"
-        >
-          {{ element.name }}
-        </div>
-      </template>
-    </draggable>
-  </p>
+  
+  <draggable 
+    v-if="players.length > 0"
+    :list="players" 
+    :disabled="!enabled"
+    item-key="name"
+    class="list-group"
+    ghost-class="ghost"
+    @start="dragging = true"
+    @end="dragging = false"
+  >
+    <template #item="{ element }">
+      <div
+        class="list-group-item"
+        :class="{ 'not-draggable': !enabled }"
+      >
+        {{ element.name }}
+      </div>
+    </template>
+  </draggable>
+  <div v-else>
+    <div>{{ t('playerSeeder.empty') }}</div>
+  </div>
+
 </template>
   
   <script setup lang="ts">
   import draggable from 'vuedraggable'
   import { onMounted, type PropType } from 'vue'
   import { ref } from 'vue'
+  import { useI18n } from 'vue-i18n';
+
+  const {t} = useI18n({})
   
   // https://github.com/SortableJS/vue.draggable.next/blob/master/example/components/simple.vue
   onMounted(async () => {
