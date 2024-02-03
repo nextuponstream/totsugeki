2616629701ff435400d1e707af7bcff0b88ea3d7
nextuponstream <nextuponstream@gmail.com>
2024-02-03 21:49:31 +0100

    Add failing test

--- tournament-organiser-web/cypress/e2e/login.cy.ts
+++ tournament-organiser-web/cypress/e2e/login.cy.ts
@@ -1,1 +1,22 @@
-// TODO warning when unknown user
+it('shows warning when user is unknown', () => {
+  cy.visit('/')
+
+  cy.get('[data-test-id=modal]').should('not.be.visible')
+  cy.contains('Register').click()
+  cy.get('[data-test-id=modal]').should('be.visible')
+  cy.contains('Email')
+  cy.contains('Password')
+
+  cy.intercept('POST', '/api/login').as('login')
+
+  cy.get('[name=login]').within(() => {
+    cy.get('[name=email]').type('unknown@user.ch')
+    cy.get('[name=password]').type('securePass123#')
+    cy.get('button').click()
+  })
+
+  cy.wait('@login').then((interception) => {
+    assert.equal(interception.response?.statusCode, 404)
+  })
+  cy.contains('Unknown email')
+})
