f092e8a3d34bfeeda90c7755dfb9ac392fbf04bf
nextuponstream <nextuponstream@gmail.com>
2024-02-10 20:18:32 +0100

    Enable failing tests

--- tournament-organiser-web/cypress/e2e/quickBracket.cy.ts
+++ tournament-organiser-web/cypress/e2e/quickBracket.cy.ts
@@ -1,11 +1,20 @@
-// describe('allow creating brackets without signing up', () => {
-//   it("as an unregistered user, I can create bracket but with a warning that it won't be saved", () => {
-//     throw new Error('implement')
-//   })
-//   it('if I create an account and log in', () => {
-//     throw new Error('implement')
-//   })
-//   it('the current bracket I was managing is registered in my history', () => {
-//     throw new Error('implement')
-//   })
-// })
+describe('allow creating brackets without signing up', () => {
+  it("as an unregistered user, I can create bracket but with a warning that it won't be saved", () => {
+    throw new Error('implement')
+  })
+  it('if I create an account and log in', () => {
+    throw new Error('implement')
+  })
+  it('the current bracket I was managing is registered in my history', () => {
+    throw new Error('implement')
+  })
+})
+
+describe('creating brackets as a registered user', () => {
+  it('as registered user, I can create bracket', () => {
+    throw new Error('implement')
+  })
+  it('the current bracket I was managing is registered in my history', () => {
+    throw new Error('implement')
+  })
+})
