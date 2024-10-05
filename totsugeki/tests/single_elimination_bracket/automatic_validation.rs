5c80d88a85d6d9b7e076bc6ade83d91f0fa32127
nextuponstream <nextuponstream@gmail.com>
2024-10-05 11:20:48 +0200

    WIP

--- totsugeki/tests/single_elimination_bracket/automatic_validation.rs
+++ totsugeki/tests/single_elimination_bracket/automatic_validation.rs
@@ -4,10 +4,12 @@ use crate::single_elimination_bracket::{
 use totsugeki::bracket::seeding::Seeding;
 use totsugeki::next_opponent::NextOpponentInBracket;
 use totsugeki::opponent::Opponent;
-use totsugeki::player::Player;
+use totsugeki::player::{Participants, Player};
 use totsugeki::single_elimination_bracket::progression::ProgressionSEB;
 use totsugeki::single_elimination_bracket::SingleEliminationBracket;
+use totsugeki::ID;
 
+// FIXME duplicate definition
 fn assert_players_play_each_other(
     player_1: usize,
     player_2: usize,
@@ -34,8 +36,35 @@ fn assert_players_play_each_other(
     );
 }
 
+// FIXME duplicate definition
+fn assert_players_play_each_other_ids(
+    player_1: usize,
+    player_2: usize,
+    player_ids: &[ID],
+    bracket: &SingleEliminationBracket,
+) {
+    let (next_opponent, match_id_1) = bracket
+        .next_opponent(player_ids[player_1])
+        .expect("next opponent");
+    let Opponent::Player(next_opponent) = next_opponent else {
+        panic!("expected player")
+    };
+    assert_eq!(next_opponent, player_ids[player_2]);
+    let (next_opponent, match_id_2) = bracket
+        .next_opponent(player_ids[player_2])
+        .expect("next opponent");
+    let Opponent::Player(next_opponent) = next_opponent else {
+        panic!("expected player")
+    };
+    assert_eq!(next_opponent, player_ids[player_1]);
+
+    assert_eq!(
+        match_id_1, match_id_2,
+        "expected player to be playing the same match"
+    );
+}
 #[test]
-fn run_3_man() {
+fn run_3_man_automated() {
     let mut p = vec![Player::new("don't use".into())]; // padding for readability
     let mut seeding = vec![];
     for i in 1..=3 {
@@ -57,6 +86,40 @@ fn run_3_man() {
     let (bracket, _, new_matches) = bracket
         .tournament_organiser_reports_result(p[1].get_id(), (0, 2), p[2].get_id())
         .expect("bracket");
+    assert!(bracket.matches_to_play().is_empty());
+    assert!(new_matches.is_empty());
+    assert!(bracket.is_over());
+}
+
+#[test]
+fn run_3_man_automated2() {
+    let mut seeding = Participants::default();
+    let mut player_ids = vec![ID::new_v4()]; // padding
+    for i in 1..=3 {
+        let player = Player::new(format!("p{i}"));
+        player_ids.push(player.get_id());
+        seeding = seeding.add_participant(player).expect("updated seeding");
+    }
+    let seeding = seeding
+        .get_players_list()
+        .iter()
+        .map(Player::get_id)
+        .collect::<Vec<_>>();
+    let bracket = SingleEliminationBracket::create(Seeding::new(seeding).unwrap(), true);
+
+    assert_eq!(bracket.get_matches().len(), 2);
+    assert_eq!(bracket.matches_to_play().len(), 1);
+    assert_players_play_each_other_ids(2, 3, &player_ids, &bracket);
+    let (bracket, _, new_matches) = bracket
+        .tournament_organiser_reports_result(player_ids[2], (2, 0), player_ids[3])
+        .expect("matches");
+    assert_ne!(bracket.get_matches()[0].get_winner(), Opponent::Unknown);
+    assert_eq!(new_matches.len(), 1, "grand finals match generated");
+    assert_players_play_each_other_ids(1, 2, &player_ids, &bracket);
+    assert_eq!(bracket.matches_to_play().len(), 1);
+    let (bracket, _, new_matches) = bracket
+        .tournament_organiser_reports_result(player_ids[1], (0, 2), player_ids[2])
+        .expect("matches");
     assert!(bracket.matches_to_play().is_empty());
     assert!(new_matches.is_empty());
     assert!(bracket.is_over());
@@ -131,6 +194,49 @@ fn run_5_man_bracket_automated() {
     assert_no_next_match_after_tournament_is_over(&bracket);
 }
 
+#[test]
+fn run_5_man_bracket_automated_3() {
+    let mut bad_seeding = Participants::default();
+    let mut player_ids = vec![ID::new_v4()]; // padding
+    for i in 1..=5 {
+        let player = Player::new(format!("p{i}"));
+        player_ids.push(player.get_id());
+        bad_seeding = bad_seeding
+            .add_participant(player)
+            .expect("updated seeding");
+    }
+    let seeding = bad_seeding
+        .get_players_list()
+        .iter()
+        .map(Player::get_id)
+        .collect::<Vec<_>>();
+
+    let bracket = SingleEliminationBracket::create(Seeding::new(seeding).unwrap(), true);
+    assert_eq!(bracket.get_matches().len(), 4);
+    assert_eq!(bracket.matches_to_play().len(), 2);
+    let (bracket, _, _new_matches) = bracket
+        .tournament_organiser_reports_result(player_ids[4], (2, 0), player_ids[5])
+        .expect("bracket");
+    assert_eq!(bracket.matches_to_play().len(), 2);
+    let (bracket, _, _new_matches) = bracket
+        .tournament_organiser_reports_result(player_ids[2], (0, 2), player_ids[3])
+        .expect("bracket");
+    assert_eq!(bracket.matches_to_play().len(), 1);
+    let (bracket, _, _new_matches) = bracket
+        .tournament_organiser_reports_result(player_ids[1], (2, 0), player_ids[4])
+        .expect("bracket");
+    assert_eq!(bracket.matches_to_play().len(), 1);
+    let (bracket, _, _new_matches) = bracket
+        .tournament_organiser_reports_result(player_ids[1], (2, 0), player_ids[3])
+        .expect("bracket");
+    if !bracket.is_over() {
+        for m in bracket.get_matches() {
+            println!("{m}");
+        }
+        panic!("expected bracket to be over")
+    }
+    assert_eq!(bracket.matches_to_play().len(), 0);
+}
 #[test]
 fn bracket_8_man_automated() {
     let mut p = vec![Player::new("don't use".into())];
