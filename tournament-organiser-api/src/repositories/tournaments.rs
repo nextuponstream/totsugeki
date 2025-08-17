d97a374a9a2da52308639b2e740a63273df1574a
nextuponstream <nextuponstream@gmail.com>
2025-08-17 16:43:58 +0200

    WIP

--- tournament-organiser-api/src/repositories/tournaments.rs
+++ tournament-organiser-api/src/repositories/tournaments.rs
@@ -40,37 +40,37 @@ mod tests {
 
     #[sqlx::test(fixtures("3_players_tournament.sql"))]
     async fn deleting_tournaments_deletes_related_data(db: PgPool) {
-        let mut conn = db.acquire().await.unwrap();
+        let mut transaction = db.begin().await.unwrap();
+        // TODO refactor to repository
         let tournaments = sqlx::query("SELECT * FROM tournaments")
-            .fetch_all(&mut *conn)
+            .fetch_all(&mut *transaction)
             .await
             .unwrap();
         assert_eq!(1, tournaments.len());
+        // TODO refactor to repository
         let matches = sqlx::query("SELECT * FROM matches")
-            .fetch_all(&mut *conn)
+            .fetch_all(&mut *transaction)
             .await
             .unwrap();
         assert_eq!(5, matches.len());
-        conn.close().await.unwrap();
 
-        let mut transaction = db.begin().await.unwrap();
         let tournament_id = "62aefc4c-d6ec-4c2f-98f0-b639688cbe0c".parse().unwrap();
         TournamentRepository::delete(&mut transaction, tournament_id)
             .await
             .unwrap();
-        transaction.commit().await.unwrap();
 
-        let mut conn = db.acquire().await.unwrap();
+        // TODO refactor to repository
         let tournaments = sqlx::query("SELECT * FROM tournaments")
-            .fetch_all(&mut *conn)
+            .fetch_all(&mut *transaction)
             .await
             .unwrap();
         assert_eq!(0, tournaments.len());
+        // TODO refactor to repository
         let matches = sqlx::query("SELECT * FROM matches")
-            .fetch_all(&mut *conn)
+            .fetch_all(&mut *transaction)
             .await
             .unwrap();
         assert_eq!(0, matches.len());
-        conn.close().await.unwrap();
+        transaction.commit().await.unwrap();
     }
 }
